22bdad7321890f7b5e5144a206cf9b82597e820d
Aaron Morris <aaron.r.morris3@gmail.com>
2022-12-01 15:20:25 -0600

    addition of Student Services

--- src/main/java/com/example/springbootapi/SpringbootApiApplication.java
+++ src/main/java/com/example/springbootapi/SpringbootApiApplication.java
@@ -1,17 +1,12 @@
 package com.example.springbootapi;
 
-import com.example.springbootapi.models.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
+
+
 
 @SpringBootApplication
-@RestController
 public class SpringbootApiApplication {
 
     public static void main(String[] args) {
@@ -20,21 +15,6 @@ public class SpringbootApiApplication {
 
     }
 
-    @GetMapping("/")
-    public List<Student> hello(){
-        return List.of(
-                new Student(1L,
-                        "Mason",
-                        "mason.wudtree@gmail.com",
-                        LocalDate.of(
-                                2000,
-                                Month.JANUARY,
-                                5),
-                                21)
-
-
-                );
-    }
 
 
 
